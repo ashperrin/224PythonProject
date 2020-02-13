fff92a7ef7338ccf3f0bde38939be58883f956b8
gabe <gabesv100@gmail.com>
2020-02-13 17:59:02 -0600

    fix

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from selenium.webdriver.common.keys import Keys # for sending keys
 from selenium.webdriver.chrome.options import Options # for adding headless option
 
 def main():
-
-
-   # options = Options()
-   # options.headless = False # make true if you do not want to not want to see the browser
-    driver = webdriver.Chrome()
+    options = Options()
+    options.headless = False # make true if you do not want to not want to see the browser
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get('https://www.amazon.com')
 
     input = "fallout 4 game of the year"
